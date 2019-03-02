2d924143b3b17869d804673da9a2f8b087cb7d26
deimors <deimors@gmail.com>
2019-03-02 15:47:18 -0600

    Changed state to _remainingTime [green]

--- Assets/Code/Model/Selling/HotDogCart.cs
+++ Assets/Code/Model/Selling/HotDogCart.cs
@@ -5,27 +5,35 @@ namespace Assets.Code.Model.Selling
 {
 	public class HotDogCart
 	{
+		private readonly TimeSpan _sellTime;
 		private readonly ISubject<HotDogCartEvent> _events = new Subject<HotDogCartEvent>();
 
-		private bool _sold;
+		private TimeSpan? _remainingTime;
 
 		public HotDogCart(TimeSpan sellTime)
 		{
-			
+			_sellTime = sellTime;
 		}
 
 		public IObservable<HotDogCartEvent> Events => _events;
 
 		public void Sell()
 		{
-			_sold = true;
+			_remainingTime = _sellTime;
 		}
 
 		public void Wait(TimeSpan duration)
 		{
-			if (!_sold) return;
+			if (!_remainingTime.HasValue)
+				return;
 
-			_sold = false;
+			_remainingTime -= duration;
+
+			if (_remainingTime > TimeSpan.Zero)
+				return;
+
+			_remainingTime = null;
+			
 			_events.OnNext(new HotDogInABunSoldEvent());
 		}
 	}
