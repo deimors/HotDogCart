ef320d04104fd93172ef03dac9e07ae2fb256858
deimors <deimors@gmail.com>
2019-03-02 15:27:26 -0600

    Split SellHotDogInABun test into SellHotDogInABunAndDontWait and SellHotDogInABunAndWaitOneMinute [red]

--- Assets/Code/Model/Selling.Tests/HotDogCartTests.cs
+++ Assets/Code/Model/Selling.Tests/HotDogCartTests.cs
@@ -6,27 +6,48 @@ namespace Assets.Code.Model.Selling.Tests
 {
 	public class HotDogCartTestFixture
 	{
-		private readonly HotDogCart _pos = new HotDogCart();
+		private readonly HotDogCart _pos;
 		private readonly IObserver<HotDogCartEvent> _observer = Substitute.For<IObserver<HotDogCartEvent>>();
 
-		public HotDogCartTestFixture()
+		public HotDogCartTestFixture(TimeSpan sellTime)
 		{
+			_pos = new HotDogCart(sellTime);
 			_pos.Events.Subscribe(_observer);
 		}
 
 		protected void Act_Sell()
 			=> _pos.Sell();
 
+		protected void Act_Wait(TimeSpan duration)
+			=> _pos.Wait(duration);
+
 		protected void Assert_EventObserved(HotDogCartEvent expected)
 			=> _observer.Received().OnNext(expected);
+
+		protected void Assert_EventNotObserved(HotDogCartEvent prohibited)
+			=> _observer.DidNotReceive().OnNext(prohibited);
 	}
 
 	public class HotDogCartTests : HotDogCartTestFixture
 	{
+		public HotDogCartTests() : base(sellTime: TimeSpan.FromMinutes(1))
+		{
+		}
+
 		[Test]
-		public void SellHotDogInABun()
+		public void SellHotDogInABunAndDontWait()
 		{
 			Act_Sell();
+
+			Assert_EventNotObserved(new HotDogInABunSoldEvent());
+		}
+
+		[Test]
+		public void SellHotDogInABunAndWaitOneMinute()
+		{
+			Act_Sell();
+
+			Act_Wait(TimeSpan.FromMinutes(1));
 
 			Assert_EventObserved(new HotDogInABunSoldEvent());
 		}
--- Assets/Code/Model/Selling/HotDogCart.cs
+++ Assets/Code/Model/Selling/HotDogCart.cs
@@ -6,11 +6,22 @@ namespace Assets.Code.Model.Selling
 	public class HotDogCart
 	{
 		private readonly ISubject<HotDogCartEvent> _events = new Subject<HotDogCartEvent>();
+
+		public HotDogCart(TimeSpan sellTime)
+		{
+			
+		}
+
 		public IObservable<HotDogCartEvent> Events => _events;
 
 		public void Sell()
 		{
 			_events.OnNext(new HotDogInABunSoldEvent());
+		}
+
+		public void Wait(TimeSpan duration)
+		{
+			
 		}
 	}
 }
