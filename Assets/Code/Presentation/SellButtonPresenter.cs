827237a37b89b9682679df9bcd2f39d03d3e1d80
deimors <deimors@gmail.com>
2019-03-03 09:26:08 -0600

    Toggle Sell Button interactable on SaleStarted and Sold events

--- Assets/Code/Presentation/SellButtonPresenter.cs
+++ Assets/Code/Presentation/SellButtonPresenter.cs
@@ -1,14 +1,32 @@
 ﻿using Assets.Code.Model.Selling;
+using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using Zenject;
 
 namespace Assets.Code.Presentation
 {
 	public class SellButtonPresenter : MonoBehaviour, IPointerClickHandler
 	{
+		public Button SellButton;
+
 		[Inject]
-		public HotDogCart Cart { get; private set; }
+		public HotDogCart Cart { private get; set; }
+
+		[Inject]
+		public void Initialize()
+		{
+			Cart.Events
+				.OfType<HotDogCartEvent, SaleStartedEvent>()
+				.TakeUntilDestroy(gameObject)
+				.Subscribe(_ => SellButton.interactable = false);
+
+			Cart.Events
+				.OfType<HotDogCartEvent, HotDogInABunSoldEvent>()
+				.TakeUntilDestroy(gameObject)
+				.Subscribe(_ => SellButton.interactable = true);
+		}
 
 		public void OnPointerClick(PointerEventData eventData)
 		{
