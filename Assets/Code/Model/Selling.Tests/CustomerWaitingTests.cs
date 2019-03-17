dd219949c3346aac964309eff38d9c65986e9cb0
deimors <deimors@gmail.com>
2019-03-17 09:18:21 -0600

    Extracted CustomersTestFixture from CustomerWaitingTests

--- Assets/Code/Model/Selling.Tests/CustomerWaitingTests.cs
+++ Assets/Code/Model/Selling.Tests/CustomerWaitingTests.cs
@@ -1,30 +1,7 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Assets.Code.Model.Selling.Tests
 {
-	public abstract class CustomersTestFixture : ObserverTestFixture<CustomersEvent>
-	{
-		private Customers _customers;
-		protected abstract int LineLength { get; }
-
-		[SetUp]
-		public override void Setup()
-		{
-			_customers = new Customers(LineLength);
-
-			base.Setup();
-		}
-
-		protected override IObservable<CustomersEvent> Observable => _customers.Events;
-
-		protected void Arrange_SaleStarted()
-			=> _customers.CartObserver.OnNext(new SaleStartedEvent());
-
-		protected void Act_AddWaitingCustomer()
-			=> _customers.AddWaitingCustomer();
-	}
-
 	public class CustomerWaitingTests : CustomersTestFixture
 	{
 		protected override int LineLength => 2;
