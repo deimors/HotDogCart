314326a33144e55dc8687e83392d23142c93dd5c
deimors <deimors@gmail.com>
2019-03-20 20:03:30 -0600

    Storing remaining cook times in array, moved progress calculation into index loop [green]

--- Assets/Code/Model/Selling/Grill.cs
+++ Assets/Code/Model/Selling/Grill.cs
@@ -10,26 +10,30 @@ namespace Assets.Code.Model.Selling
 		private readonly ISubject<GrillEvent> _events = new Subject<GrillEvent>();
 		public IObservable<GrillEvent> Events => _events;
 
-		private TimeSpan? _remainingCookTime;
+		private readonly TimeSpan?[] _remainingCookTimes = new TimeSpan?[2];
+
 		private static readonly TimeSpan CookTime = TimeSpan.FromMinutes(5);
 
 		private int _nextIndex;
 
 		public void AddHotDog()
 		{
-			_remainingCookTime = CookTime;
+			var index = _nextIndex++;
+			_remainingCookTimes[index] = CookTime;
 
-			_events.OnNext(new HotDogAddedEvent(_nextIndex++));
+			_events.OnNext(new HotDogAddedEvent(index));
 		}
 
 		public void ProgressTime(TimeSpan duration)
 		{
-			_remainingCookTime -= duration;
-
-			var progress = 1 - ((double)(_remainingCookTime?.Ticks ?? 0) / CookTime.Ticks);
-
 			foreach (var index in Enumerable.Range(0, _nextIndex))
 			{
+				if (!_remainingCookTimes[index].HasValue) continue;
+
+				_remainingCookTimes[index] -= duration;
+
+				var progress = 1 - ((double)(_remainingCookTimes[index]?.Ticks ?? 0) / CookTime.Ticks);
+
 				_events.OnNext(new CookingProgressedEvent(index, (float)progress));
 
 				if (progress >= 1)
