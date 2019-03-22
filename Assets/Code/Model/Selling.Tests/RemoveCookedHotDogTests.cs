c88618945d3da3151bbc078d47d91c445558a493
deimors <deimors@gmail.com>
2019-03-21 19:47:28 -0600

    CookTwoHotDogsAndRemoveBoth test [red]

--- Assets/Code/Model/Selling.Tests/RemoveCookedHotDogTests.cs
+++ Assets/Code/Model/Selling.Tests/RemoveCookedHotDogTests.cs
@@ -24,5 +24,28 @@ namespace Assets.Code.Model.Selling.Tests
 				new CookedHotDogRemovedEvent(0)
 			);
 		}
+
+		[Test]
+		public void CookTwoHotDogsAndRemoveBoth()
+		{
+			Act_AddHotDog();
+			Act_AddHotDog();
+
+			Act_ProgressTime(CookTime);
+
+			Act_RemoveCookedHotDog();
+			Act_RemoveCookedHotDog();
+
+			Assert_EventsObserved(
+				new HotDogAddedEvent(0),
+				new HotDogAddedEvent(1),
+				new CookingProgressedEvent(0, 1),
+				new HotDogCookedEvent(0),
+				new CookingProgressedEvent(1, 1),
+				new HotDogCookedEvent(1),
+				new CookedHotDogRemovedEvent(0),
+				new CookedHotDogRemovedEvent(1)
+			);
+		}
 	}
 }
