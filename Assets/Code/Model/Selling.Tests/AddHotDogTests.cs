86af0db14022e3ba041546a87ae4985846852f6f
deimors <deimors@gmail.com>
2019-03-22 22:11:08 -0600

    AddHotDogTests with AddThreeHotDogs test [red]

---
+++ Assets/Code/Model/Selling.Tests/AddHotDogTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Assets.Code.Model.Selling.Events;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Assets.Code.Model.Selling.Tests
+{
+	public class AddHotDogTests : GrillTestFixture
+	{
+		private static readonly TimeSpan HalfCookTime = TimeSpan.FromMinutes(2.5);
+		protected override TimeSpan CookTime => TimeSpan.FromMinutes(5);
+
+		[Test]
+		public void AddThreeHotDogs()
+		{
+			Act_AddHotDog();
+			Act_AddHotDog();
+			Act_AddHotDog();
+
+			Assert_EventObserved(Arg.Any<HotDogAddedEvent>(), 2);
+		}
+	}
+}
