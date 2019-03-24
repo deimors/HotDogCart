5b689710a3de3033e5a2b3b9035b3b331a59aa2f
deimors <deimors@gmail.com>
2019-03-23 18:18:13 -0600

    CookedHotDogAvailabilityTests with AvailableWhenOneCookedCompletely test [red]

--- Assets/Code/Model/Selling.Tests/RemoveCookedHotDogTests.cs
+++ Assets/Code/Model/Selling.Tests/RemoveCookedHotDogTests.cs
@@ -137,4 +137,23 @@ namespace Assets.Code.Model.Selling.Tests
 			);
 		}
 	}
+
+	public class CookedHotDogAvailabilityTests : GrillTestFixture
+	{
+		private static readonly TimeSpan HalfCookTime = TimeSpan.FromMinutes(2.5);
+		protected override TimeSpan CookTime => TimeSpan.FromMinutes(5);
+
+		[Test]
+		public void AvailableWhenOneCookedCompletely()
+		{
+			Act_AddHotDog();
+
+			Act_ProgressTime(CookTime);
+
+			Assert_EventsObserved(
+				new HotDogCookedEvent(0),
+				new CookedHotDogAvailableEvent()
+			);
+		}
+	}
 }
