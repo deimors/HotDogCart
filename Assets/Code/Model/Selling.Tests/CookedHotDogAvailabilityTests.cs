8223baed28af819f51919ba9532329de062889ab
deimors <deimors@gmail.com>
2019-03-23 18:24:04 -0600

    CookTwoCompletely availability test [red]

---
+++ Assets/Code/Model/Selling.Tests/CookedHotDogAvailabilityTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Assets.Code.Model.Selling.Events;
+using NUnit.Framework;
+
+namespace Assets.Code.Model.Selling.Tests
+{
+	public class CookedHotDogAvailabilityTests : GrillTestFixture
+	{
+		private static readonly TimeSpan HalfCookTime = TimeSpan.FromMinutes(2.5);
+		protected override TimeSpan CookTime => TimeSpan.FromMinutes(5);
+
+		[Test]
+		public void CookOneCompletely()
+		{
+			Act_AddHotDog();
+
+			Act_ProgressTime(CookTime);
+
+			Assert_EventsObserved(
+				new HotDogCookedEvent(0),
+				new CookedHotDogAvailableEvent()
+			);
+		}
+
+		[Test]
+		public void CookTwoCompletely()
+		{
+			Act_AddHotDog();
+			Act_AddHotDog();
+
+			Act_ProgressTime(CookTime);
+
+			Assert_EventsObserved(
+				new HotDogCookedEvent(0),
+				new CookedHotDogAvailableEvent(),
+				new HotDogCookedEvent(1)
+			);
+		}
+	}
+}
--- Assets/Code/Model/Selling.Tests/RemoveCookedHotDogTests.cs
+++ Assets/Code/Model/Selling.Tests/RemoveCookedHotDogTests.cs
@@ -137,23 +137,4 @@ namespace Assets.Code.Model.Selling.Tests
 			);
 		}
 	}
-
-	public class CookedHotDogAvailabilityTests : GrillTestFixture
-	{
-		private static readonly TimeSpan HalfCookTime = TimeSpan.FromMinutes(2.5);
-		protected override TimeSpan CookTime => TimeSpan.FromMinutes(5);
-
-		[Test]
-		public void AvailableWhenOneCookedCompletely()
-		{
-			Act_AddHotDog();
-
-			Act_ProgressTime(CookTime);
-
-			Assert_EventsObserved(
-				new HotDogCookedEvent(0),
-				new CookedHotDogAvailableEvent()
-			);
-		}
-	}
 }
