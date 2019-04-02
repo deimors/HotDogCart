87f1cb786012078159e5a0ad287abe954c4fbb99
deimors <deimors@gmail.com>
2019-04-01 19:15:54 -0600

    CanAddHotDogTests with AddTwoHotDogs test [red]

---
+++ Assets/Code/Model/Selling.Tests/CanAddHotDogTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Assets.Code.Model.Selling.Events;
+using NUnit.Framework;
+
+namespace Assets.Code.Model.Selling.Tests
+{
+	public class CanAddHotDogTests : GrillTestFixture
+	{
+		private static readonly TimeSpan HalfCookTime = TimeSpan.FromMinutes(2.5);
+		protected override TimeSpan CookTime { get; }
+
+		[Test]
+		public void AddTwoHotDogs()
+		{
+			Act_AddHotDog();
+			Act_AddHotDog();
+
+			Assert_EventObserved(new CantAddHotDogEvent());
+		}
+	}
+}
