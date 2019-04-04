2c3fd18c3696f6256a2f245b86d5a7ad39e736b1
deimors <deimors@gmail.com>
2019-04-03 20:08:26 -0600

    Integrated CanAddHotDogEvent and CantAddHotDogEvent with AddButtonPresenter

--- Assets/Code/Presentation/AddButtonPresenter.cs
+++ Assets/Code/Presentation/AddButtonPresenter.cs
@@ -1,4 +1,6 @@
 ﻿using Assets.Code.Model.Selling;
+using Assets.Code.Model.Selling.Events;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -16,6 +18,14 @@ namespace Assets.Code.Presentation
 		public void Initialize()
 		{
 			AddButton.onClick.AddListener(() => Grill.AddHotDog());
+
+			var enableStream = Grill.Events.OfType<GrillEvent, CanAddHotDogEvent>().Select(_ => true);
+			var disableStream = Grill.Events.OfType<GrillEvent, CantAddHotDogEvent>().Select(_ => false);
+
+			enableStream
+				.Merge(disableStream)
+				.TakeUntilDestroy(gameObject)
+				.Subscribe(e => AddButton.interactable = e);
 		}
 	}
 }
