d19a30b1455d1b4b903dbf74afddaf46a47870b0
deimors <deimors@gmail.com>
2019-04-17 20:10:28 -0600

    Emit TimeProgressedEvent with expected duration [green]

--- Assets/Code/Model/Selling/Time.cs
+++ Assets/Code/Model/Selling/Time.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using Assets.Code.Model.Selling.Events;
+﻿using Assets.Code.Model.Selling.Events;
+using System;
+using UniRx;
 
 namespace Assets.Code.Model.Selling
 {
 	public class Time
 	{
-		public IObservable<TimeEvent> Events { get; }
+		private readonly ISubject<TimeEvent> _events = new Subject<TimeEvent>();
+		public IObservable<TimeEvent> Events => _events;
 
 		public void Progress(TimeSpan duration)
 		{
-			
+			_events.OnNext(new TimeProgressedEvent(TimeSpan.FromMinutes(3)));
 		}
 	}
 }
