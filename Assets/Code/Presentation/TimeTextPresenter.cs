1b5a086e74c4230487b001931c34fe0d532a0425
deimors <deimors@gmail.com>
2019-04-18 19:24:28 -0600

    Emit TimeProgressedEvent with expected CurrentTime (devil's advocate) [green]

---
+++ Assets/Code/Presentation/TimeTextPresenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Assets.Code.Model.Selling.Events;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using Time = Assets.Code.Model.Selling.Time;
+
+namespace Assets.Code.Presentation
+{
+	public class TimeTextPresenter : MonoBehaviour
+	{
+		public Text TimeText;
+
+		public DateTime StartDate;
+
+		[Inject]
+		public void Initialize(Time time)
+		{
+			time.Events
+				.OfType<TimeEvent, TimeProgressedEvent>()
+				.Select(e => (StartDate + e.Duration).ToString("HH:mm:ss tt"))
+				.Subscribe(currentTime => TimeText.text = currentTime);
+		}
+	}
+}
